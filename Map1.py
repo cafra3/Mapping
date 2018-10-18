730831b7db04b6a43a64a255bb92c36446373e64
Calvin Francis <calfrancis94@gmail.com>
2018-10-17 22:15:33 -0400

    Layers and Control Panel

--- Map1.py
+++ Map1.py
@@ -23,13 +23,25 @@ Height: %s m
 
 map = folium.Map(location=[38.58, -99.09],zoom_start=5,tiles="Mapbox Bright")
 
-fg = folium.FeatureGroup(name="My Map")
+
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for la,lo,el,na in zip(latitude,longitude,elevation,name):
     iframe = folium.IFrame(html=html % (na,na,el), width=200, height=100)
     # fg.add_child(folium.Marker(location=[la,lo], popup=folium.Popup(iframe), icon=folium.Icon(color=markercolor(el)))) #regular markers
-    fg.add_child(folium.CircleMarker(location=[la,lo], popup=folium.Popup(iframe), fill_color=markercolor(el), color=markercolor(el), radius=4, fill_opacity=0.7))
+    fgv.add_child(folium.CircleMarker(location=[la,lo], popup=folium.Popup(iframe), fill_color=markercolor(el), color=markercolor(el), radius=4, fill_opacity=0.7))
 
-map.add_child(fg)
+
+
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open('world.json','r',encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 1000000 <= x['properties']['POP2005'] < 20000000 else 'red'})) #country layer
+
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
